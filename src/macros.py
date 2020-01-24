378a41a7e60f26e520d05a6eb279db4a09b8e442
KEDIARAHUL135 <rahul.kedia@airavath.in>
2020-01-24 15:57:52 +0530

    Add feature to find masked image
    Masked image for white and yellow colours are found.

--- src/macros.py
+++ src/macros.py
@@ -5,3 +5,14 @@
 # Project       : SegmentLaneLines
 # Description   : This file contains the variables/macros for main.py.
 ################################################################################
+
+import numpy as np
+
+
+# WaitKey value
+WAITKEY_VALUE = 0
+
+
+# Lower and upper bounds for masking
+LOWER_BOUND = np.array([0, 0, 180])
+UPPER_BOUND = np.array([255, 255, 255])
--- src/main.py
+++ src/main.py
@@ -9,17 +9,60 @@
 import cv2
 import numpy as np
 import os
+import src.macros as M
 
 
+################################################################################
+# Function      : FindMaskImage
+# Parameters    : InputImage - Holds the image to be processed.
+#                 HSVImage - Holds hsv form of input image.
+#                 MaskImage - Holds the image after masking.
+# Description   : This function finds the mask image of the original image
+#                 for yellow and white areas showing road lines. But other
+#                 similar coloured objects are also included like air.
+# Return        : MaskImage
+################################################################################
+def FindMaskImage(InputImage):
+    HSVImage = cv2.cvtColor(InputImage, cv2.COLOR_BGR2HSV)
+    MaskImage = cv2.inRange(HSVImage, M.LOWER_BOUND, M.UPPER_BOUND)
+    cv2.imshow("Mask", MaskImage)
+    return MaskImage
+
+
+############################################################ ####################
+# Function      : ProcessImage
+# Parameters    : InputImage - Holds the image to be processed.
+#                 MaskImage - Holds the image after masking.
+# Description   :
+# Return        :
+################################################################################
+def ProcessImage(InputImage):
+    MaskImage = FindMaskImage(InputImage)
+
+
+################################################################################
+# Function      : ReadInputAndProcess
+# Parameters    : InputImagesFolderPath - This contains the path of
+#                                         InputImages folder
+#                 ImageName - Name of image to be read
+#                 InputImage - Input image to be processed
+#                 KeyPressed - read the key pressed by the user
+# Description   : This function reads all images present in InputImages folder
+#                 one by one and passes them for processing and breaks the
+#                 process when spacebar is pressed.
+# Return        : -
+################################################################################
 def ReadInputAndProcess():
     InputImagesFolderPath = os.path.abspath(os.path.join('InputImages'))
 
-    for ImagePath in os.listdir(InputImagesFolderPath):
-        InputImage = cv2.imread(InputImagesFolderPath + '/' + ImagePath)
+    for ImageName in os.listdir(InputImagesFolderPath):
+        InputImage = cv2.imread(InputImagesFolderPath + '/' + ImageName)
         InputImage = cv2.resize(InputImage, (842, 486))
         cv2.imshow("InputImage", InputImage)
 
-        KeyPressed = cv2.waitKey(0)
+        ProcessImage(InputImage)
+
+        KeyPressed = cv2.waitKey(M.WAITKEY_VALUE)
 
         if KeyPressed == 32:      # Break when "Spacebar" is pressed
             break
