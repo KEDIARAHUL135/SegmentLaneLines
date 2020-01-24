0f9cd092ad5c3f3ad52debef947fcb30d639b4ce
KEDIARAHUL135 <rahul.kedia@airavath.in>
2020-01-24 11:40:47 +0530

    Add feature too read all input images
    Images present are read one by one and the process is breaked
    when spacebar key is pressed.

--- src/macros.py
+++ src/macros.py
@@ -1,1 +1,7 @@
-#
+###############################################################################
+# File          : macros.py
+# Created by    : Rahul Kedia
+# Created on    : 24/01/2020
+# Project       : SegmentLaneLines
+# Description   : This file contains the variables/macros for main.py.
+################################################################################
--- src/main.py
+++ src/main.py
@@ -1,1 +1,28 @@
-#
+###############################################################################
+# File          : main.py
+# Created by    : Rahul Kedia
+# Created on    : 24/01/2020
+# Project       : SegmentLaneLines
+# Description   : This file contains the main source code for the project.
+################################################################################
+
+import cv2
+import numpy as np
+import os
+
+
+def ReadInputAndProcess():
+    InputImagesFolderPath = os.path.abspath(os.path.join('InputImages'))
+
+    for ImagePath in os.listdir(InputImagesFolderPath):
+        InputImage = cv2.imread(InputImagesFolderPath + '/' + ImagePath)
+        InputImage = cv2.resize(InputImage, (842, 486))
+        cv2.imshow("InputImage", InputImage)
+
+        KeyPressed = cv2.waitKey(0)
+
+        if KeyPressed == 32:      # Break when "Spacebar" is pressed
+            break
+
+
+ReadInputAndProcess()
